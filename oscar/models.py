97e25a492aae8b9fe3369c07eef932957521447d
nabil alaa <nabilalaa06@gmail.com>
2025-03-20 19:14:48 +0200

    oscar

--- oscar/models.py
+++ oscar/models.py
@@ -3,43 +3,45 @@ from django.contrib.auth.models import User
 
 
 class Movie(models.Model):
-    name = models.CharField(max_length=150, null=True)
-    describe = models.TextField(null=True)
-    category = models.CharField(max_length=150, null=True)
-    date_production = models.IntegerField(null=True)
-    rate = models.CharField(max_length=5,null=True)
-    durations = models.IntegerField(null=True)
-    creator = models.CharField(max_length=150, null=True)
-    stars = models.CharField(max_length=150, null=True)
-    image = models.ImageField(upload_to="Movies",null=True)
-    imageUrl = models.URLField(verbose_name="imageurl",null=True)
-    platform = models.CharField(max_length=50, null=True)
-    published = models.DateTimeField(auto_now_add=True)
-    video = models.URLField(null=True)
+    name = models.CharField(max_length=150, null=True, blank=True)
+    describe = models.TextField(null=True, blank=True)
+    category = models.CharField(max_length=150, null=True, blank=True)
+    date_production = models.IntegerField(null=True, blank=True)
+    rate = models.CharField(max_length=5, null=True, blank=True)
+    durations = models.IntegerField(null=True, blank=True)
+    creator = models.CharField(max_length=150, null=True, blank=True)
+    stars = models.CharField(max_length=150, null=True, blank=True)
+    image = models.ImageField(upload_to="Movies", null=True, blank=True)
+    imageUrl = models.URLField(verbose_name="imageurl", null=True, blank=True)
+
+    platform = models.CharField(max_length=50, null=True, blank=True)
+    published = models.DateTimeField(auto_now=True, blank=True)
+    video = models.URLField(null=True,blank=True)
+    iframe = models.URLField(null=True,blank=True)
 
     def __str__(self):
         return self.name
 
-    class Meta():
+    class Meta:
         ordering = ["-published"]
 
 
 class Show(models.Model):
-    name = models.CharField(max_length=150, null=True)
-    describe = models.TextField(null=True)
-    category = models.CharField(max_length=150, null=True)
-    date_production = models.IntegerField(null=True)
-    rate = models.CharField(max_length=5,null=True)
-    durations = models.IntegerField(null=True)
-    creator = models.CharField(max_length=150, null=True)
-    stars = models.CharField(max_length=150, null=True)
-    image = models.ImageField(upload_to="Series",null=True)
-    imageUrl = models.URLField(verbose_name="imageurl",null=True)
+    name = models.CharField(max_length=150, null=True, blank=True)
+    describe = models.TextField(null=True, blank=True)
+    category = models.CharField(max_length=150, null=True, blank=True)
+    date_production = models.IntegerField(null=True, blank=True)
+    rate = models.CharField(max_length=5, null=True, blank=True)
+    durations = models.IntegerField(null=True, blank=True)
+    creator = models.CharField(max_length=150, null=True, blank=True)
+    stars = models.CharField(max_length=150, null=True, blank=True)
+    image = models.ImageField(upload_to="Series", null=True, blank=True)
+    imageUrl = models.URLField(verbose_name="imageurl", null=True, blank=True)
 
-    platform = models.CharField(max_length=50, null=True)
-    published = models.DateTimeField(auto_now=True)
+    platform = models.CharField(max_length=50, null=True, blank=True)
+    published = models.DateTimeField(auto_now=True, blank=True)
 
-    class Meta():
+    class Meta:
         ordering = ["-published"]
 
     def __str__(self):
@@ -48,7 +50,9 @@ class Show(models.Model):
 
 class Episode(models.Model):
     name = models.ForeignKey(Show, null=True, on_delete=models.CASCADE)
-    video = models.URLField(null=True)
+    video = models.URLField(null=True, blank=True)
+    iframe = models.URLField(null=True, blank=True)
+
     number = models.IntegerField(null=True)
 
     def __str__(self):
@@ -66,7 +70,7 @@ class Banner(models.Model):
 class Favourite(models.Model):
     name = models.CharField(max_length=150, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    rate = models.CharField(max_length=5,null=True)
+    rate = models.CharField(max_length=5, null=True)
     image = models.ImageField(null=True)
 
     def __str__(self):
