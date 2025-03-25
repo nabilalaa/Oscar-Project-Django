dd1ba3e015304cd2c40805012ddbf4f331362afb
nabil alaa <nabilalaa06@gmail.com>
2025-03-25 18:25:36 +0200

    oscar

--- project/settings.py
+++ project/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": "neondb",
         "USER": "neondb_owner",
         "PASSWORD": "npg_6wcsMfdx5nUu",
